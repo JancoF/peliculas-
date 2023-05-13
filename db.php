c1e5c1ad024abfcd28e53bb1313ac77ceec7e89b
Noe <noe.flores@tecsup.edu.pe>
2023-05-12 22:23:34 -0500

    subiendo nnuvos cambios

--- db.php
+++ db.php
@@ -2,9 +2,9 @@
 session_start();
 
 $conn = mysqli_connect(
-  'localhost',
-  'root',
-  '',
+  'clhlei1e7005is6ahi280kx72:9000',
+  'clhlei1e2001oahs62qw148k0',
+  'KvsMK0W6yYRKHxIh6uanR3FE',
   'peliculas'
 ) or die(mysqli_error($mysqli));
 
