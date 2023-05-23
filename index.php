21ad00484429d10713c7faf75a7d62b9826ab74c
JancoF <129468210+JancoF@users.noreply.github.com>
2023-05-22 22:34:42 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php include("db.php"); ?>
+<?php ./path("db.php"); ?>
 
-<?php include("Componentes/header.php"); ?>
+<?php ./path("Componentes/header.php"); ?>
 
 <main class="container p-4">
   <div class="row">
