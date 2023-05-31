214c4b15fe454dd088f3e4859762490a0a01ed97
branyerbjr <branyer.romero@tecsup.edu.pe>
2023-05-31 14:44:17 -0500

    conexion a mysql

--- db.php
+++ db.php
@@ -2,9 +2,9 @@
 session_start();
 
 $conn = mysqli_connect(
-  'clhlei1e7005is6ahi280kx72:9000',
-  'clhlei1e2001oahs62qw148k0',
-  'KvsMK0W6yYRKHxIh6uanR3FE',
+  'clhlei1e7005is6ahi280kx72:3306',
+  'clhlei1e4001pahs67vovh4pz',
+  'PIZJneg1FCTmEww0mYwW6Nl5',
   'peliculas'
 ) or die(mysqli_error($mysqli));
 
